6de578e92422da24cc6b69b14d7447694b6be285
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-10-12 01:24:36 +0530

    Edited ls

--- Assignment-1/src/ls.c
+++ Assignment-1/src/ls.c
@@ -8,7 +8,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 int ls(char *dirName, int option_a, int option_1) {
-    if (dirName == "") {
+    if (dirName == NULL || strcmp(dirName, "") == 0) {
         char *dirName = (char *) malloc(256*sizeof(char));
         getcwd(dirName, 256);
     }
