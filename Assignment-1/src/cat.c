f1a1966ad529e277391db859927396b18e9bc584
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-10-12 23:58:05 +0530

    Added cat

--- Assignment-1/src/cat.c
+++ Assignment-1/src/cat.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char *argv[]) {
-    printf("OH MA G INSIDE %s \n", argv[0]);
+    int *options = (int *) calloc(256, sizeof(int));
+    int args = 0;
+
+    for (int i=1; i < argc; i++) {
+        if (argv[i][0] != '-') {
+            args++;
+            continue;
+        }
+        for (int j=1; j < strlen(argv[i]); j++) {
+            char option = argv[i][j];
+            if (option != 'n' && option != 'E') {
+                printf("cat: invalid option -- '%c' \n", option);
+                return 1;
+            }
+            options[option]++;
+        }
+    }
+
+    if (args == 0) {
+        int maxSize = 256;
+        char *input = (char *) malloc(maxSize*sizeof(char));
+
+        getline(&input, &maxSize, stdin);
+        while (&input != EOF) {
+            puts(input);
+            getline(&input, &maxSize, stdin);
+        }
+
+        return 0;
+    }
+
     return 0;
 }
+// -n -E
