4ce99f03bd15af7153a497bfbc2a0687be6d1691
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-10-12 12:09:05 +0530

    Edited date

--- Assignment-1/src/date.c
+++ Assignment-1/src/date.c
@@ -12,7 +12,8 @@ void getDate() {
     char *buffer = (char *) malloc(256*sizeof(char));
 
     struct tm *ts = localtime(&now);
-    strftime(buffer, 256, "%a %Y-%m-%d %H:%M:%S %Z", ts);
+    // strftime(buffer, 256, "%a %Y-%m-%d %H:%M:%S %Z", ts);
+    strftime(buffer, 256, "%a %b %d %H:%M:%S %p %Z %Y", ts);
     printf("%s\n", buffer);
 }
 
