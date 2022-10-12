ff78323933e0b3ff816513e24628527405f1bb3e
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-10-13 00:53:20 +0530

    Edits

--- Assignment-1/src/cat.c
+++ Assignment-1/src/cat.c
@@ -4,6 +4,38 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+int cat(char *filename, int option_n, int option_E) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("cat: %s: No such file or directory \n", filename);
+        return 1;
+    }
+
+    int read;
+    int counter = 1;
+    size_t maxSize = 256;
+    char *line = (char *) malloc(maxSize*sizeof(char));
+
+    while ((read = getline(&line, &maxSize, file)) != -1) {
+        if (option_n > 0) {
+            printf("\t%d  ", counter++);
+        }
+
+        line[read-1] = ((line[read-1] == '\n') ? '\0' : line[read-1]);
+        printf("%s", line);
+
+        if (option_E > 0) {
+            printf("$");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 int main(int argc, char *argv[]) {
     int *options = (int *) calloc(256, sizeof(int));
     int args = 0;
@@ -40,6 +72,12 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
+    int retSum = 0;
+    for (int i=1; i < argc; i++) {
+        if (argv[i][0] != '-') {
+            retSum += cat(argv[i], options['n'], options['E']);
+        }
+    }
+
     return 0;
 }
-// -n -E
