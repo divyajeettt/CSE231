259d6299ed661845b356b05f4173190efc442ccb
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-10-12 12:04:28 +0530

    Edited date

--- Assignment-1/src/date.c
+++ Assignment-1/src/date.c
@@ -5,15 +5,20 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 void getDate() {
-    time_t datetime;
-    time(&datetime);
-    printf("%s \n", ctime(&datetime));
+    time_t now;
+    time(&now);
+    printf("%s", ctime(&now));
+
+    char *buffer = (char *) malloc(256*sizeof(char));
+
+    struct tm *ts = localtime(&now);
+    strftime(buffer, sizeof(buffer), "%a %Y-%m-%d %H:%M:%S %Z", ts);
+    printf("%s\n", buffer);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char *argv[]) {
     getDate();
-    // printf("OH MA G INSIDE %s \n", argv[0]);
     return 0;
 }
