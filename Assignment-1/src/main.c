9c8366e70131f92accd017a5e53a227e35e06f49
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-10-14 22:04:41 +0530

    Debugging echo -e

--- Assignment-1/src/main.c
+++ Assignment-1/src/main.c
@@ -102,7 +102,7 @@ char *escape(char *string) {
     int len = strlen(string) - 1;
     printf("received stirngn = %s, len= %d \n", string, len);
     for (int i=0; i < len; i++) {
-        if (i < len-1 && string[i] == '\\') {
+        if (string[i] == '\\') {
             printf("string[i] = %c, trying to Matcg character: %c \n", string[i], string[i+1]);
             if (string[i+1] == 'a') {
                 escaped[i] = '\a';
