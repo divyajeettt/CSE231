4d1b3f9fb715151c1e9e5f567cafcf409c978c4c
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-11-14 17:55:00 +0530

    Edited Bash scripts

--- Assignment-2/1/1.2/buildA.sh
+++ Assignment-2/1/1.2/buildA.sh
@@ -1,21 +1,19 @@
-echo "oh ma g"
+mkdir ./kernelbuildA
 
-# mkdir ./kernelbuildA
+cd ./kernelbuildA
 
-# cd ./kernelbuildA
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.8.tar.xz
 
-# wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.8.tar.xz
+unxz linux-5.19.8.tar.xz
 
-# unxz linux-5.19.8.tar.xz
+tar -xvf linux-5.19.8.tar
 
-# tar -xvf linux-5.19.8.tar
+chown -R $dvgt:$dvgt linux-5.19.8
 
-# chown -R $dvgt:$dvgt linux-5.19.8
+cd linux-5.19.8
 
-# cd linux-5.19.8
+make mrproper
 
-# make mrproper
+cp ./../../1/1.2/.config .config
 
-# cp ./../../1/1.2/.config .config
-
-# make -j$(nproc)
+make -j$(nproc)
