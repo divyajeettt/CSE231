824e82cd8d94229f791f68cfafbe651c2e7d8e3e
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-11-17 18:08:01 +0530

    Refactored benchmarking

---
+++ Assignment-2/1/1.1/Benchmarks/plotter.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import csv
+
+
+def plot() -> None:
+    with open(r"./1/1.1/benchmark.csv") as file:
+        reader = csv.reader(file, delimiter="\t")
+        data = list(reader)
+
+    benchmarks = []
+    for i in range(len(data)):
+        benchmarks.append({
+            data[i][0]: float(data[i][1]),
+            data[i][2]: float(data[i][3]),
+            data[i][4]: float(data[i][5]),
+        })
+        del data[i][-1]
+
+    # print(benchmarks)
+
+    Thr_A = [benchmarks[i]["A"] for i in range(len(benchmarks))]
+    Thr_B = [benchmarks[i]["B"] for i in range(len(benchmarks))]
+    Thr_C = [benchmarks[i]["C"] for i in range(len(benchmarks))]
+
+    plt.figure(figsize=(10, 5))
+
+    plt.axhline(y=0, color="black", linewidth=2)
+    plt.axvline(x=0, color="black", linewidth=2)
+
+    plt.plot(range(1, len(benchmarks)+1), Thr_A, marker=".", label="A", color="#971B00")
+    plt.plot(range(1, len(benchmarks)+1), Thr_B, marker=".", label="B", color="#001787")
+    plt.plot(range(1, len(benchmarks)+1), Thr_C, marker=".", label="C", color="#007B07")
+
+    plt.axhline(y=sum(Thr_A)/len(Thr_A), label="avg(A)", linestyle="--", color="#FF2E00")
+    plt.axhline(y=sum(Thr_B)/len(Thr_B), label="avg(B)", linestyle="--", color="#002BFF")
+    plt.axhline(y=sum(Thr_C)/len(Thr_C), label="avg(C)", linestyle="--", color="#00FF0F")
+
+    plt.xlabel("Number of trials")
+    plt.xticks(range(1, len(benchmarks)+1))
+    plt.ylabel("Time of execution (in seconds)")
+    plt.yticks(range(1, 11))
+
+    plt.title("Benchmark with priorities: A=0, B=1, C=1")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot()
