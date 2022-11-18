82bcd77c2b3a32026d8fe757e02fd34650f04915
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-11-18 14:44:36 +0530

    Edited bash-scripts

--- Assignment-2/1/1.2/buildA.sh
+++ Assignment-2/1/1.2/buildA.sh
@@ -1,5 +1,5 @@
-cp -r ./linux-5.19.8 ./1.2/kernelbuild/A/linux-5.19.8
+cp -r ./linux-5.19.8 ./1/1.2/kernelbuild/A/linux-5.19.8
 
-cd ./1.2/kernelbuild/A/linux-5.19.8
+cd ./1/1.2/kernelbuild/A/linux-5.19.8
 
 make -j$(nproc)
