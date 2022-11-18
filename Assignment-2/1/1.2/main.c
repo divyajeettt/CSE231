3060c5574ff871fae61148fd91242482a0023392
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-11-18 15:38:22 +0530

    Added benchmarking

--- Assignment-2/1/1.2/main.c
+++ Assignment-2/1/1.2/main.c
@@ -1,14 +1,32 @@
 #include <time.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <sys/wait.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+void writeToFile(FILE *file, char *str, double time)
+{
+    char buffer[100];
+    sprintf(buffer, "%f", time);
+
+    fprintf(file, str);
+    fprintf(file, "\t");
+    fprintf(file, buffer);
+    fprintf(file, "\t");
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
+    FILE *file = fopen("./1/1.2/Benchmarks/benchmark.csv", "a");
+
     struct timespec start, end;
     double time;
+
+    struct sched_param param;
 
     clock_gettime(CLOCK_REALTIME, &start);
     int pid1 = fork();
@@ -22,9 +40,14 @@ int main()
         }
         return 0;
     }
+    param.sched_priority = 99;
+    sched_setscheduler(pid1, SCHED_OTHER, &param);
+
     waitpid(pid1, NULL, 0);
     clock_gettime(CLOCK_REALTIME, &end);
+
     time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+    writeToFile(file, "A", time);
     printf("Time taken by 1st Process: %f seconds \n", time);
 
     clock_gettime(CLOCK_REALTIME, &start);
@@ -39,9 +62,14 @@ int main()
         }
         return 0;
     }
+    param.sched_priority = 99;
+    sched_setscheduler(pid1, SCHED_RR, &param);
+
     waitpid(pid2, NULL, 0);
     clock_gettime(CLOCK_REALTIME, &end);
+
     time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+    writeToFile(file, "B", time);
     printf("Time taken by 2nd Process: %f seconds \n", time);
 
     clock_gettime(CLOCK_REALTIME, &start);
@@ -56,10 +84,17 @@ int main()
         }
         return 0;
     }
+    param.sched_priority = 99;
+    sched_setscheduler(pid1, SCHED_FIFO, &param);
+
     waitpid(pid3, NULL, 0);
     clock_gettime(CLOCK_REALTIME, &end);
     time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+    writeToFile(file, "C", time);
     printf("Time taken by 3rd Process: %f seconds \n", time);
+
+    fprintf(file, "\n");
+    fclose(file);
 
     return 0;
 }
