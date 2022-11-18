5bdf0a6978c4f8d415dfc4f86a83c0756201d2b4
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-11-18 12:27:33 +0530

    Updates

--- Assignment-2/1/1.1/Benchmarks/plotter.py
+++ Assignment-2/1/1.1/Benchmarks/plotter.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import csv
 
 
@@ -22,14 +23,30 @@ def plot() -> None:
     Thr_B = [benchmarks[i]["B"] for i in range(len(benchmarks))]
     Thr_C = [benchmarks[i]["C"] for i in range(len(benchmarks))]
 
+    A_priorities = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    B_priorities = [1, 10, 1 ,1, 1, 1, 1, 1, 1, 1]
+    C_priorities = [1, 1, 20, 1, 1, 1, 1, 1, 1, 1]
+
     plt.figure(figsize=(10, 5))
 
-    plt.axhline(y=0, color="black", linewidth=2)
-    plt.axvline(x=0, color="black", linewidth=2)
+    plt.axhline(y=0, color="black", linewidth=2, zorder=3)
+    plt.axvline(x=0, color="black", linewidth=2, zorder=3)
 
-    plt.plot(range(1, len(benchmarks)+1), Thr_A, marker=".", label="Thr-A", color="#971B00")
-    plt.plot(range(1, len(benchmarks)+1), Thr_B, marker=".", label="Thr-B", color="#001787")
-    plt.plot(range(1, len(benchmarks)+1), Thr_C, marker=".", label="Thr-C", color="#007B07")
+    plt.bar(np.arange(1-0.2, len(benchmarks)+1-0.2, 1), Thr_A, width=0.2, label="Thr-A", color="#971B00", zorder=2)
+    plt.bar(np.arange(1, len(benchmarks)+1, 1), Thr_B, width=0.2, label="Thr-B", color="#001787", zorder=2)
+    plt.bar(np.arange(1+0.2, len(benchmarks)+1+0.2, 1), Thr_C, width=0.2, label="Thr-C", color="#007B07", zorder=2)
+
+    # # write A priorities on top of bars
+    # for i in range(len(A_priorities)):
+    #     plt.text(i+1-0.2, 0.25, A_priorities[i], ha="center", va="bottom", color="white", size=9, zorder=4)
+
+    # # write B priorities on top of bars
+    # for i in range(len(B_priorities)):
+    #     plt.text(i+1, 0.25, B_priorities[i], ha="center", va="bottom", color="white", size=9, zorder=4)
+
+    # # write C priorities on top of bars
+    # for i in range(len(C_priorities)):
+    #     plt.text(i+1+0.2, 0.25, C_priorities[i], ha="center", va="bottom", color="white", size=9, zorder=4)
 
     plt.axhline(y=sum(Thr_A)/len(Thr_A), linestyle="--", color="#FF2E00")
     plt.axhline(y=sum(Thr_B)/len(Thr_B), linestyle="--", color="#002BFF")
@@ -39,9 +56,10 @@ def plot() -> None:
     plt.xticks(range(1, len(benchmarks)+1))
     plt.ylabel("Time of execution (in seconds)")
     plt.yticks(range(1, 11))
+    plt.ylim(-1, 11)
 
-    plt.title("Benchmark with priorities: A=0, B=1, C=1")
-    plt.grid(True)
+    plt.title("Thread Priorities: A=0, B=1, C=1\nNumber of CPU Cores=1")
+    plt.grid(True, zorder=0)
     plt.legend()
     plt.show()
 
