385cd86f026834f8cb62eb5e06da76c505ae4a6c
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-11-20 18:04:23 +0530

    Added benchmarking

---
+++ Assignment-2/1/1.2/Benchmarks/plotter.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+
+def get_benchmarks() -> list[dict[str, float]]:
+    with open(r"./1/1.1/Benchmarks/benchmark.csv") as file:
+        reader = csv.reader(file, delimiter="\t")
+        data = list(reader)
+
+    benchmarks = []
+    for i in range(len(data)):
+        benchmarks.append({
+            data[i][0]: float(data[i][1]),
+            data[i][2]: float(data[i][3]),
+            data[i][4]: float(data[i][5]),
+        })
+        del data[i][-1]
+
+    return benchmarks
+
+
+def get_process_times(benchmarks: list[dict[str, float]]) -> tuple[list[float], list[float], list[float]]:
+    Pr_A = [benchmarks[i]["A"] for i in range(len(benchmarks))]
+    Pr_B = [benchmarks[i]["B"] for i in range(len(benchmarks))]
+    Pr_C = [benchmarks[i]["C"] for i in range(len(benchmarks))]
+
+    return Pr_A, Pr_B, Pr_C
+
+
+def set_figure() -> None:
+    plt.figure(figsize=(10, 5))
+    plt.axhline(y=0, color="black", linewidth=2, zorder=3)
+    plt.axvline(x=0, color="black", linewidth=2, zorder=3)
+
+
+def plot1() -> None:
+    """Plot the benchmarks for 3 processs on fixed priorities for n trials"""
+
+    Pr_A, Pr_B, Pr_C = get_process_times(get_benchmarks())
+
+    set_figure()
+
+    plt.line(np.arange(1-0.2, len(benchmarks)+1-0.2, 1), Pr_A, width=0.2, label="Pr-A", color="#971B00", zorder=2)
+    plt.line(np.arange(1, len(benchmarks)+1, 1), Pr_B, width=0.2, label="Pr-B", color="#001787", zorder=2)
+    plt.line(np.arange(1+0.2, len(benchmarks)+1+0.2, 1), Pr_C, width=0.2, label="Pr-C", color="#007B07", zorder=2)
+
+    plt.axhline(y=sum(Pr_A)/len(Pr_A), linestyle="--", color="#FF2E00")
+    plt.axhline(y=sum(Pr_B)/len(Pr_B), linestyle="--", color="#002BFF")
+    plt.axhline(y=sum(Pr_C)/len(Pr_C), linestyle="--", color="#00FF0F")
+
+    plt.xlabel("Number of Trials")
+    plt.xticks(range(1, len(benchmarks)+1))
+    plt.ylabel("Time of execution (in seconds)")
+
+    plt.title("Priorities: Pr-A=5, Pr-B=20, Pr-C=30\nNumber of cores = 4")
+    plt.grid(True, zorder=0)
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot()
--- Assignment-2/1/1.2/main.c
+++ Assignment-2/1/1.2/main.c
@@ -23,73 +23,102 @@ int main()
 {
     FILE *file = fopen("./1/1.2/Benchmarks/benchmark.csv", "a");
 
-    struct timespec start, end;
+    struct timespec startA, endA, startB, endB, startC, endC;
     double time;
 
     struct sched_param param;
 
-    clock_gettime(CLOCK_REALTIME, &start);
+    clock_gettime(CLOCK_REALTIME, &startA);
     int pid1 = fork();
-    if (pid1 == 0)
+    if (pid1 < 0)
+    {
+        perror("fork1");
+        return 1;
+    }
+    else if (pid1 == 0)
     {
         // Child process
         if (execlp("bash", "bash", "./1/1.2/buildA.sh", (char *) NULL) == -1)
         {
-            perror("execlp");
+            perror("execlp1");
             return 1;
         }
-        return 0;
+        else
+        {
+            return 0;
+        }
     }
-    param.sched_priority = 99;
-    sched_setscheduler(pid1, SCHED_OTHER, &param);
+    else
+    {
+        param.sched_priority = 5;
+        sched_setscheduler(pid1, SCHED_OTHER, &param);
 
-    waitpid(pid1, NULL, 0);
-    clock_gettime(CLOCK_REALTIME, &end);
+        clock_gettime(CLOCK_REALTIME, &startB);
+        int pid2 = fork();
+        if (pid2 < 0)
+        {
+            perror("fork2");
+            return 1;
+        }
+        else if (pid2 == 0)
+        {
+            // Child process
+            if (execlp("bash", "bash", "./1/1.2/buildB.sh", (char *) NULL) == -1)
+            {
+                perror("execlp");
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+        else
+        {
+            param.sched_priority = 20;
+            sched_setscheduler(pid1, SCHED_RR, &param);
 
-    time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+            clock_gettime(CLOCK_REALTIME, &startC);
+            int pid3 = fork();
+            if (pid3 < 0)
+            {
+                perror("fork3");
+                return 1;
+            }
+            else if (pid3 == 0)
+            {
+                // Child process
+                if (execlp("bash", "bash", "./1/1.2/buildC.sh", (char *) NULL) == -1)
+                {
+                    perror("execlp");
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else {
+                param.sched_priority = 30;
+                sched_setscheduler(pid1, SCHED_FIFO, &param);
+                wait(NULL);
+                clock_gettime(CLOCK_REALTIME, &endC);
+            }
+            wait(NULL);
+            clock_gettime(CLOCK_REALTIME, &endB);
+        }
+        wait(NULL);
+        clock_gettime(CLOCK_REALTIME, &endA);
+    }
+
+    time = (endA.tv_sec - startA.tv_sec) + (endA.tv_nsec - startA.tv_nsec) / 1000000000.0;
     writeToFile(file, "A", time);
     printf("Time taken by 1st Process: %f seconds \n", time);
 
-    clock_gettime(CLOCK_REALTIME, &start);
-    int pid2 = fork();
-    if (pid2 == 0)
-    {
-        // Child process
-        if (execlp("bash", "bash", "./1/1.2/buildB.sh", (char *) NULL) == -1)
-        {
-            perror("execlp");
-            return 1;
-        }
-        return 0;
-    }
-    param.sched_priority = 99;
-    sched_setscheduler(pid1, SCHED_RR, &param);
-
-    waitpid(pid2, NULL, 0);
-    clock_gettime(CLOCK_REALTIME, &end);
-
-    time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+    time = (endB.tv_sec - startB.tv_sec) + (endB.tv_nsec - startB.tv_nsec) / 1000000000.0;
     writeToFile(file, "B", time);
     printf("Time taken by 2nd Process: %f seconds \n", time);
 
-    clock_gettime(CLOCK_REALTIME, &start);
-    int pid3 = fork();
-    if (pid3 == 0)
-    {
-        // Child process
-        if (execlp("bash", "bash", "./1/1.2/buildC.sh", (char *) NULL) == -1)
-        {
-            perror("execlp");
-            return 1;
-        }
-        return 0;
-    }
-    param.sched_priority = 99;
-    sched_setscheduler(pid1, SCHED_FIFO, &param);
-
-    waitpid(pid3, NULL, 0);
-    clock_gettime(CLOCK_REALTIME, &end);
-    time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+    time = (endC.tv_sec - startC.tv_sec) + (endC.tv_nsec - startC.tv_nsec) / 1000000000.0;
     writeToFile(file, "C", time);
     printf("Time taken by 3rd Process: %f seconds \n", time);
 
