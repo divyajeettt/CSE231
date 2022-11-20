cdc549f6bf994fcc19c749d66c3002725b4383d6
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-11-20 18:26:08 +0530

    Edits

--- Assignment-2/1/1.2/buildB.sh
+++ Assignment-2/1/1.2/buildB.sh
@@ -1,15 +1,9 @@
-# cp -r ./linux-5.19.8 ./1/1.2/kernelbuild/B/linux-5.19.8
+cp -r ./linux-5.19.8 ./1/1.2/kernelbuild/B/linux-5.19.8
 
-# cp -r ./1/1.2/.config ./1/1.2/kernelbuild/B/linux-5.19.8/.config
+cp -r ./1/1.2/.config ./1/1.2/kernelbuild/B/linux-5.19.8/.config
 
-# cd ./1/1.2/kernelbuild/B/linux-5.19.8
+cd ./1/1.2/kernelbuild/B/linux-5.19.8
 
-# make -j$(nproc)
-
-i=1
-while [ $i -le 1000000 ]
-do
-    i=$(($i+1))
-done
+make -j$(nproc)
 
 echo "Build B done"
