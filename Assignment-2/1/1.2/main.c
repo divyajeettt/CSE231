5248449b607e912df00703ea7a4227b914e65471
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-11-24 19:31:40 +0530

    Fixed 1.2

--- Assignment-2/1/1.2/main.c
+++ Assignment-2/1/1.2/main.c
@@ -24,7 +24,7 @@ int main()
     FILE *file = fopen("./1/1.2/Benchmarks/benchmark.csv", "a");
 
     struct timespec startA, endA, startB, endB, startC, endC;
-    double time;
+    double timeA, timeB, timeC;
 
     struct sched_param param;
 
@@ -38,6 +38,7 @@ int main()
     else if (pid1 == 0)
     {
         // Child process
+        nice(-1);
         if (execlp("bash", "bash", "./1/1.2/buildA.sh", (char *) NULL) == -1)
         {
             perror("execlp1");
@@ -50,9 +51,6 @@ int main()
     }
     else
     {
-        param.sched_priority = 30;
-        sched_setscheduler(pid1, SCHED_OTHER, &param);
-
         clock_gettime(CLOCK_REALTIME, &startB);
         int pid2 = fork();
         if (pid2 < 0)
@@ -63,20 +61,20 @@ int main()
         else if (pid2 == 0)
         {
             // Child process
+            param.sched_priority = 10;
+            sched_setscheduler(pid2, SCHED_RR, &param);
             if (execlp("bash", "bash", "./1/1.2/buildB.sh", (char *) NULL) == -1)
             {
                 perror("execlp");
                 return 1;
             }
-            else {
+            else
+            {
                 return 0;
             }
         }
         else
         {
-            param.sched_priority = 99;
-            sched_setscheduler(pid1, SCHED_RR, &param);
-
             clock_gettime(CLOCK_REALTIME, &startC);
             int pid3 = fork();
             if (pid3 < 0)
@@ -87,6 +85,8 @@ int main()
             else if (pid3 == 0)
             {
                 // Child process
+                param.sched_priority = 99;
+                sched_setscheduler(pid3, SCHED_FIFO, &param);
                 if (execlp("bash", "bash", "./1/1.2/buildC.sh", (char *) NULL) == -1)
                 {
                     perror("execlp");
@@ -97,30 +97,29 @@ int main()
                     return 0;
                 }
             }
-            else {
-                param.sched_priority = 10;
-                sched_setscheduler(pid1, SCHED_FIFO, &param);
-                wait(NULL);
+            else
+            {
+                waitpid(pid3, NULL, 0);
                 clock_gettime(CLOCK_REALTIME, &endC);
+
+                timeC = (endC.tv_sec - startC.tv_sec) + (endC.tv_nsec - startC.tv_nsec) / 1000000000.0;
+                writeToFile(file, "C", timeC);
+                printf("Time taken by 3rd Process (SCHED_FIFO): %lf seconds \n", timeC);
             }
-            wait(NULL);
+            waitpid(pid2, NULL, 0);
             clock_gettime(CLOCK_REALTIME, &endB);
+
+            timeB = (endB.tv_sec - startB.tv_sec) + (endB.tv_nsec - startB.tv_nsec) / 1000000000.0;
+            writeToFile(file, "B", timeB);
+            printf("Time taken by 2nd Process (SCHED_RR): %lf seconds \n", timeB);
         }
-        wait(NULL);
+        waitpid(pid1, NULL, 0);
         clock_gettime(CLOCK_REALTIME, &endA);
+
+        timeA = (endA.tv_sec - startA.tv_sec) + (endA.tv_nsec - startA.tv_nsec) / 1000000000.0;
+        writeToFile(file, "A", timeA);
+        printf("Time taken by 1st Process (SHCED_OTHER): %lf seconds \n", timeA);
     }
-
-    time = (endA.tv_sec - startA.tv_sec) + (endA.tv_nsec - startA.tv_nsec) / 1000000000.0;
-    writeToFile(file, "A", time);
-    printf("Time taken by 1st Process: %f seconds \n", time);
-
-    time = (endB.tv_sec - startB.tv_sec) + (endB.tv_nsec - startB.tv_nsec) / 1000000000.0;
-    writeToFile(file, "B", time);
-    printf("Time taken by 2nd Process: %f seconds \n", time);
-
-    time = (endC.tv_sec - startC.tv_sec) + (endC.tv_nsec - startC.tv_nsec) / 1000000000.0;
-    writeToFile(file, "C", time);
-    printf("Time taken by 3rd Process: %f seconds \n", time);
 
     fprintf(file, "\n");
     fclose(file);
