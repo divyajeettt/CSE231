03503d3c867e6dab1254fbbd26c5e2e8eadc149a
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-12-09 21:31:42 +0530

    Added helpher functions

---
+++ Assignment-3/2/helper.h
@@ -0,0 +1,44 @@
+#define N 50
+#define CHUNK 5
+#define LENGTH 10
+#define SOCKET "./crimge.socket"
+
+
+char **generateStrings()
+{
+    char **strings = (char **) malloc(N*sizeof(char *));
+    for (int i = 0; i < N; i++)
+    {
+        strings[i] = (char *) malloc(LENGTH*sizeof(char));
+        for (int j = 0; j < LENGTH; j++)
+        {
+            strings[i][j] = 'a' + rand() % 26;
+        }
+    }
+    return strings;
+}
+
+
+int toInt(char *buffer)
+{
+    int num = 0;
+    num += ((int) (buffer[0] - '0')) * 10;
+    num += ((int) (buffer[1] - '0'));
+    return num;
+}
+
+
+char *toString(int num)
+{
+    char *buffer = (char *) malloc(LENGTH*sizeof(char));
+    memset(buffer, '0', LENGTH*sizeof(char));
+
+    if (num < 10) buffer[1] = '0' + num;
+    else
+    {
+        buffer[0] = '0' + num / 10;
+        buffer[1] = '0' + num % 10;
+    }
+
+    return buffer;
+}
