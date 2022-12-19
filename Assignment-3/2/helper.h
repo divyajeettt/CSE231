23772b383d881490df7ca32f788e58f596f0e7a3
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-12-19 10:27:02 +0530

    Added functions to handle batches/data-packets

--- Assignment-3/2/helper.h
+++ Assignment-3/2/helper.h
@@ -1,11 +1,12 @@
 #define N 50
 #define CHUNK 5
 #define LENGTH 10
-#define SOCKET "./crimge.socket"
+#define BATCH_SIZE (LENGTH+2)*CHUNK*sizeof(char)
+
 #define FIFO_S2C "./crimge-s2c.fifo"
 #define FIFO_C2S "./crimge-c2s.fifo"
+#define SOCKET "./crimge.socket"
 #define SHM "./crimge-shm"
-#define SHM_SIZE LENGTH * (CHUNK+2) * sizeof(char)
 
 
 char **generateStrings()
@@ -14,7 +15,7 @@ char **generateStrings()
     for (int i = 0; i < N; i++)
     {
         strings[i] = (char *) malloc(LENGTH*sizeof(char));
-        for (int j = 0; j < LENGTH; j++) strings[i][j] = 'a' + rand() % 26;
+        for (int j = 0; j < LENGTH; j++) strings[i][j] = 'a' + (rand() % 26);
     }
     return strings;
 }
@@ -31,8 +32,8 @@ int toInt(char *buffer)
 
 char *toString(int num)
 {
-    char *buffer = (char *) malloc(LENGTH*sizeof(char));
-    memset(buffer, '0', LENGTH*sizeof(char));
+    char *buffer = (char *) malloc(2*sizeof(char));
+    memset(buffer, '0', 2*sizeof(char));
 
     if (num < 10) buffer[1] = '0' + num;
     else
@@ -45,15 +46,49 @@ char *toString(int num)
 }
 
 
-char **parseMessage(char *buffer)
+char *addIndex(char *string, int index)
 {
-    int index = 0;
-    char **strings = (char **) malloc((CHUNK+1)*sizeof(char *));
+    char *indexed = (char *) malloc((LENGTH+2)*sizeof(char));
+    strcpy(indexed, toString(index));
+    strcat(indexed, string);
+    return indexed;
+}
 
-    for (int i=0; i < CHUNK+1; i++) {
-        strings[i] = (char *) malloc(LENGTH*sizeof(char));
-        for (int j=0; j < LENGTH; j++) strings[i][j] = buffer[index++];
+
+char **splitIndex(char *string)
+{
+    char **strings = (char **) malloc(2*sizeof(char *));
+    strings[0] = (char *) malloc(2*sizeof(char));
+    strings[1] = (char *) malloc(LENGTH*sizeof(char));
+
+    for (int i=0; i < 2; i++) strings[0][i] = string[i];
+    for (int i=0; i < LENGTH; i++) strings[1][i] = string[i+2];
+
+    return strings;
+}
+
+
+char *makeBatch(char **strings, int index)
+{
+    char *batch = (char *) malloc(BATCH_SIZE);
+    memset(batch, '\0', BATCH_SIZE);
+    for (int i=0; i < CHUNK; i++)
+    {
+        char *indexedString = addIndex(strings[index+i], index+i);
+        strcat(batch, indexedString);
     }
+    return batch;
+}
 
+
+char **splitBatch(char *batch)
+{
+    char **strings = (char **) malloc(CHUNK*sizeof(char *));
+    int index = 0;
+    for (int i=0; i < CHUNK; i++)
+    {
+        strings[i] = (char *) malloc((LENGTH+2)*sizeof(char));
+        for (int j=0; j < LENGTH+2; j++) strings[i][j] = batch[index++];
+    }
     return strings;
 }
