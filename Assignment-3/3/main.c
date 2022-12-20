5a871e813a4fc49649303cf0a857d00075fe5436
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-12-20 12:21:24 +0530

    Finished 3

---
+++ Assignment-3/3/main.c
@@ -0,0 +1,44 @@
+#include <linux/pid.h>
+#include <linux/init.h>
+#include <linux/cred.h>
+#include <linux/sched.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/moduleparam.h>
+
+
+int param_pid = 0;
+module_param(param_pid, int, S_IRUSR|S_IWUSR);
+
+
+void sys_read_task_struct(void)
+{
+    struct task_struct *task = pid_task(find_vpid(param_pid), PIDTYPE_PID);
+
+    printk("pid: %d \n", task->pid);
+    printk("user_id: %d\n", task->cred->uid);
+    printk("process group id: %d \n", task->tgid);
+    printk("community path: %s \n", task->comm);
+}
+
+
+static int __init sys_read_task_struct_init(void)
+{
+    printk(KERN_ALERT "init invoked [run: param_pid = %d] \n", param_pid);
+    sys_read_task_struct();
+    return 0;
+}
+
+
+static void __exit sys_read_task_struct_exit(void)
+{
+    printk(KERN_ALERT "exit invoked \n");
+}
+
+
+module_init(sys_read_task_struct_init);
+module_exit(sys_read_task_struct_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("dvgt");
+MODULE_DESCRIPTION("Kernel Module implemented for CSE231 Assignment-3");
